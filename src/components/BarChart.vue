42b26c25ad049061be8214eacecbf9e31276adaf
Stav <papadokolos@gmail.com>
2023-06-05 17:48:07 +0300

    Create Bar chart from hardcoded json

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,16 +1,103 @@
 <template>
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+    <Bar id="my-chart-id" :data="chartData" :options="chartOptions" />
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import Papa from 'papaparse'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-const parsedCSV = ref([
+const parsedCSV = ref([])
+
+const pricesByProduct = ref(null)
+const pricesDatasets = ref([])
+
+function groupBy(objs, key) {
+    return objs.reduce(function (r, a) {
+        r[a[key]] = r[a[key]] || []
+        r[a[key]].push(a)
+        return r
+    }, Object.create(null))
+}
+
+function removeDuplicates(objs, keys = null) {
+    if (!objs) {
+        return null
+    }
+
+    if (keys) {
+        return objs.filter((v,i,a)=>a.findIndex(v2=>keys.every(k=>v2[k] ===v[k]))===i)
+    }
+    else {
+        return objs.filter((v,i,a)=>a.findIndex(v2=>(JSON.stringify(v2) === JSON.stringify(v)))===i)
+    }
+}
+
+function filterOldPrices(prices) {
+    let filteredPrices = []
+
+    let pricesByLocation = groupBy(prices, 'מקום')
+    Object.entries(pricesByLocation).forEach(([location, locationPrices]) => {
+        let productPrices = groupBy(locationPrices, 'מוצר')
+        Object.entries(productPrices).forEach(([product, productPrices]) => {
+            if (productPrices) {
+                let newestPrice = takeNewestPrice(productPrices)
+                filteredPrices.push(newestPrice)
+            }
+        })
+    })
+
+    return filteredPrices
+}
+
+function takeNewestPrice(pricesByLocation) {
+    let newestDate = new Date(pricesByLocation[0]['תאריך'])
+    let newestPriceObject = pricesByLocation[0]
+
+    pricesByLocation.forEach((priceObject) => {
+        let date = new Date(priceObject['תאריך'])
+        if (date > newestDate) {
+            newestDate = date
+            newestPriceObject = priceObject
+        }
+    })
+
+    return newestPriceObject
+}
+
+watch(parsedCSV, (newCSV, oldCSV) => {
+    let prices = removeDuplicates(newCSV)
+    prices = filterOldPrices(prices)
+    prices = prices.map((price) => {
+        price['מחיר'] = price['מחיר'].slice(1)
+        return price
+    })
+    pricesByProduct.value = groupBy(prices, 'מוצר')
+})
+
+watch(pricesByProduct, (newPrices, oldPrices) => {
+    let chartDatasets = []
+
+    if (newPrices) {
+        Object.entries(newPrices).forEach(([product, productPrices]) => {
+            let dataset = {
+                label: product,
+                data: productPrices,
+                backgroundColor: dynamicColors()
+            }
+
+            chartDatasets.push(dataset)
+        })
+    }
+
+    console.log(chartDatasets)
+    pricesDatasets.value = chartDatasets
+})
+
+parsedCSV.value = [
     {
         "מחיר": "₪10.00",
         "קטגוריה": "שתייה",
@@ -80,8 +167,26 @@ const parsedCSV = ref([
         "מקום": "פביאנו",
         "מוצר": "פיצה",
         "תאריך": "05/06/2023"
+    },
+    {
+        "מחיר": "₪15.00",
+        "קטגוריה": "מאפים",
+        "מקום": "פביאנו",
+        "מוצר": "סלט",
+        "תאריך": "01/06/2023"
     }
-])
+]
+
+function dynamicColors() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function formatNumberToNIS(price) {
+    return new Intl.NumberFormat('he-IL', { style: 'currency', currency: 'ILS' }).format(price)
+}
 
 // const parsedCSV = ref(null)
 
@@ -99,12 +204,50 @@ export default {
     name: 'BarChart',
     components: { Bar },
     computed: {
-        chartData() {
-            return {
-                labels: parsedCSV.value.map(item => item['מקום']),
-                datasets: [ { data: parsedCSV.value.map(item => item['מחיר'].slice(1)) }]
-            } },
-        chartOptions() { return { responsive: true} }
+        chartData() { 
+            return { datasets: pricesDatasets.value }
+        },
+        chartOptions() { return {
+            responsive: true,
+            parsing: {
+                xAxisKey: 'מקום',
+                yAxisKey: 'מחיר'
+            },
+            plugins: {
+                legend: {
+                    rtl: true,
+                    textDirection: 'rtl'
+                },
+                tooltip: {
+                    callbacks: {
+                        label: (context) => {
+                            let label = context.dataset.label || ''
+
+                            if (label) {
+                                label += ': '
+                            }
+                            if (context.parsed.y !== null) {
+                                label += formatNumberToNIS(context.parsed.y)
+                            }
+                            return label
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    stacked: true
+                },
+                y: {
+                    ticks: {
+                        callback: (value, index, ticks) => {
+                            return formatNumberToNIS(value)
+                        }
+                    },
+                    stacked: true
+                }
+            }
+        }}
     }
 }
 </script>
